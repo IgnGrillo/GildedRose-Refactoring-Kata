5144af68c8c2479c2f735a9a64c775ee31400b71
IgnGrillo <igngrillo@gmail.com>
2023-06-11 20:41:27 -0300

    Update GildedRose test and implementation, adding the test: DontReduceTheQualityBelowZeroWhenEndDay

--- csharp/GildedRoseShould.cs
+++ csharp/GildedRoseShould.cs
@@ -41,7 +41,21 @@ namespace csharp
 
             ThenAssertQualityValue(initialQuality - 1, randomItem);
         }
-        
+
+        [Test]
+        public void DontReduceTheQualityBelowZeroWhenEndDay()
+        {
+            const int sellIn = 0;
+            const int initialQuality = 0;
+            var randomItem = GivenAnItemWith("Random Item", sellIn, initialQuality);
+            IList<Item> items = GivenAListOfItemsWith(randomItem);
+            GivenAGildedRoseWithItems(items);
+
+            WhenEndDay();
+
+            ThenAssertQualityIsGreaterOrEqualThanZero(randomItem);
+        }
+
         [Test]
         public void ReduceTheQualityByTwoWhenEndDayAndSellInIsBelowOrEqualToZeroAndInitialQualityIsGreaterThanTwo()
         {
@@ -70,7 +84,10 @@ namespace csharp
 
         private static void ThenAssertSellInValue(int expectedSellIn, Item item) => 
                 Assert.AreEqual(expectedSellIn, item.SellIn);
-        
+
+        private static void ThenAssertQualityIsGreaterOrEqualThanZero(Item item) => 
+                Assert.GreaterOrEqual(item.Quality, 0);
+
         private static void ThenAssertQualityValue(int expectedQuality, Item item) => 
                 Assert.AreEqual(expectedQuality, item.Quality);
     }
